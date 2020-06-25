fdfba8a4d221604d56a481a7845d7a91c8510ac0
Kiran Chhatre <kiranchhatre3@gmail.com>
2020-06-25 19:07:23 +0200

    added sf_light_ip_dir path

--- src/main/python/calibration/BEAMPyOpt/search-space-control/config.py
+++ src/main/python/calibration/BEAMPyOpt/search-space-control/config.py
@@ -9,6 +9,8 @@ base_urbansim_config = beam+ '/test/input/sf-light/urbansim-10k.conf'
 copy_urbansim_config = beam+ '/test/input/sf-light/urbansim-10k_%d.conf'
 copy_urbansim_txt = beam +'/test/input/sf-light/urbansim-10k_%d.txt'
 
+sf_light_ip_dir = beam + '/test/input/sf-light' 
+
 sf_light_dir = beam + '/output/sf-light/*'
 
 output_csv=f'/{shared}/%d_%d.csv'
