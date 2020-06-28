b2eb76b1ddb4aebd417635e432fdd87b3f2d6363
Kiran Chhatre <kiranchhatre3@gmail.com>
2020-06-28 19:05:07 +0200

    added procedure for clearing BEAM urbansim op dir

--- src/main/python/calibration/BEAMPyOpt/search-space-control/prepare_new_SSC_run.py
+++ src/main/python/calibration/BEAMPyOpt/search-space-control/prepare_new_SSC_run.py
@@ -4,7 +4,8 @@ import os, subprocess, glob
 '''
 1. Deletes all *.log file in BEAM dir
 2. Deletes all urbansim-10k_*.conf or txt files from sf-light folder
-3. Deletes all files except 1_*.csv from the storage folder
+3. Deletes all files except 1_*.csv from the storage 
+4. Delete BEAM urbansim output directory to clear up space on EC2 before the run
 '''
 
 for item in os.listdir(beam):
@@ -19,3 +20,8 @@ bashCommand = "find . \! -name '1_*.csv' -a \! -name '*.py' -a \! -type d -delet
 subprocess.Popen(bashCommand, shell=True, executable='/bin/bash') # point 3
 os.chdir(search_space)
 print('Ready for a fresh SSC run!')
+
+# point 4
+files = glob.glob(sf_light_dir)
+for f in files:
+    os.remove(f)
